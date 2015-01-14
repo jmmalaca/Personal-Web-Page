e931d0da3c7a263214cf10f38e170cb53e6e208e
tOOnPT <jose.miguel.malaca@me.com>
2015-01-14 17:16:01 +0000

    Update project... ready to continue... tweetsData added.

---
+++ ViewModels/Blog.js
@@ -0,0 +1,33 @@
+﻿//Connect to the blog and resquest data...
+function GetHtmlBlogData() {
+    var blogID = "2110626611689613378";
+    var key = "AIzaSyBt-RNvjfvthZri5tAbc6bgFi53P2AqPQ8";
+    var theUrl = "https://www.googleapis.com/blogger/v3/blogs/" + blogID + "/posts?maxResults=1&key=" + key;
+    
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    if (window.XMLHttpRequest) {
+        xmlHttp = new XMLHttpRequest();
+    }
+    else {
+        // code for IE6, IE5
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.setRequestHeader("dataType", "jsonp");
+    xmlHttp.send();
+    var resp = JSON.parse(xmlHttp.responseText);
+    var items = resp.items;
+    $("#Blog-Box").append(items[0].content);
+}
+
+//document ready event ----------
+$(document).ready(function() {
+
+    //add div-box
+    $("#Blog-Page").append("<div id=\"BlogTitle\"> my last <b>post</b>... </div>");
+    $("#Blog-Page").append("<div id=\"Blog-Box\"></div>");
+
+    //set data...
+    GetHtmlBlogData();
+});
