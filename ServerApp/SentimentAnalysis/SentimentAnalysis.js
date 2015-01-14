465456ebf189327935e6bcb110ba97a075fc60dc
tOOnPT <jose.miguel.malaca@me.com>
2015-01-14 13:27:19 +0000

    save it...

---
+++ ServerApp/SentimentAnalysis/SentimentAnalysis.js
@@ -0,0 +1,39 @@
+﻿//Sentiment Analysis System
+var DataReader = require('../SentimentData/DataReader.js');
+
+(function() {
+    
+    "use strict";
+    // Define your library strictly...
+
+    function SentimentAnalysis() {
+
+        this.filePath = "";
+
+        //Private Methods
+        function SetupData() {
+            console.log(" -SA reading initial data...");
+
+        }
+
+        function Train() {
+            console.log(" -SA training step...");
+        }
+        
+        //Public Methods
+        this.Start = function() {
+            console.log(" -Start SA system...");
+
+            //Read Data...
+            var reader = new DataReader();
+            reader.Read();
+
+            //Train... or read System trained...
+
+            //Setup system for classification...
+        }
+    }
+
+    //Export the SA System Object
+    module.exports = SentimentAnalysis;
+}());
--- ServerApp/server.js
+++ ServerApp/server.js
@@ -1,94 +1,43 @@
-﻿var express = require('express'); //express module
-var request = require('request'); //request module
-var url = require('url'); //url module
-var OAuth = require('OAuth');//oauth module
+﻿var express = require('express'); //express npm module
+var request = require('request'); //request npm module
+var url = require('url'); //url npm module
+var SentimentAnalysis = require('./SentimentAnalysis/SentimentAnalysis.js');
 
-//get a express app...
+//get a express app started...
 var serverApp = express();
 
 //route definitio:... home
 serverApp.get('/', function (req, response) {
-    console.log("\nHome Call...");
+    console.log("\n -Hello request");
 
-    //just send a hello world string :D
-    response.send('Hello World');
+    request(url, function(err, res, body) {
+        response.render('hello.ejs');//file on views folder
+    });
 });
 
-//Route definition: /tweets/username
-serverApp.get('/tweets/:searchString', function(req, response) {
-        console.log("\nTweets Call...");
+//Route definition: /tweets/searchString
+serverApp.get('/sentiment/:searchString', function (req, response) {
+    console.log("\n -Tweets Call...");
+    
+    //get the request string data...
+    var searchString = req.params.searchString;
+    console.log(" -Search String: " + searchString);
 
-        //get the request string data...
-        var searchString = req.params.searchString;
-        console.log(" -Search String: " + searchString);
-
-        //var oauth = new OAuth.OAuth(
-        //    'https://api.twitter.com/oauth/request_token',
-        //    'https://api.twitter.com/oauth/access_token',
-        //    'pxza4SS4CRtD8uSSzQLgTA',
-        //    'cLBhzg65BmtoDZAXMAZvFOHK2nIMlzx277fk335YnGQ',
-        //    '1.0A',
-        //    null,
-        //    'HMAC-SHA1'
-        //);
-
-        //oauth.get(
-        //    'https://api.twitter.com/1.1/search/tweets.json?q=' + searchString + '&lang=eng&count=10',
-        //    '91327431-7IwG2LsOtc62JU3StzdcIImdylq8TYr86YS517P0I', 
-        //    'fy2TBb0cI3UdmTweQtOPHIXtKW5fGPNF2jGGUPnyPS8', 
-        //    function (e, data, res) {
-        //        if (e) console.error('ERROR: ' + e);
-        //        console.log(require('util').inspect(data));
-        //});
-
-        //options = {
-        //    protocol: 'https:',
-        //    host: 'api.twitter.com',
-        //    pathname: '/1.1/search/tweets.json?q=' + searchString + '&lang=eng&count=10',
-        //    oauth: {
-        //        oauth_consumer_key: 'pxza4SS4CRtD8uSSzQLgTA',
-        //        oauth_consumer_secret: 'cLBhzg65BmtoDZAXMAZvFOHK2nIMlzx277fk335YnGQ',
-        //        token: '91327431-7IwG2LsOtc62JU3StzdcIImdylq8TYr86YS517P0I',
-        //        tokensecret: 'fy2TBb0cI3UdmTweQtOPHIXtKW5fGPNF2jGGUPnyPS8'
-        //    }
-        //}
-
-        //var twitterUrl = url.format(options);
-        //request(twitterUrl).pipe(response);
-
-        //var oauth2 = new OAuth.OAuth2(
-        //    'pxza4SS4CRtD8uSSzQLgTA',
-        //    'cLBhzg65BmtoDZAXMAZvFOHK2nIMlzx277fk335YnGQ',
-        //    'https://api.twitter.com/',
-        //    null,
-        //    'oauth2/token',
-        //    null
-        //);
-        
-        //oauth2.getOAuthAccessToken(
-        //    '',
-        //    { 'grant_type': 'client_credentials' },
-        //    function(e, access_token, refresh_token, results) {
-        //        console.log('bearer: ', access_token);
-        //        oauth2.get('protected url',
-        //        access_token, function(e, data, res) {
-                
-        //            if (e) response.send(e);
-
-        //            if (res.statusCode != 200)
-        //                response.send('OAuth2 request failed: ' + res.statusCode);
-
-        //            try {
-        //                data = JSON.parse(data);
-        //            } catch (e) {
-        //                response.send(e);
-        //            }
-
-        //            response.send(e + ' | ' + data);
-        //        });
-        //});
+    //Call Tweeter...
+    var connector = require('./TwitterConnector/TwitterConnector.js');
 });
 
 //Start listen on port...
-serverApp.listen(8080);
-console.log("8080 is the magic port");
+var server = serverApp.listen(8080, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    if (host == "0.0.0.0")
+        host = "localhost";
+    console.log('\nApp listening at http://%s:%s', host, port);
+});
+
+//Sentiment Analysis System Up and ready...
+var system = new SentimentAnalysis();
+system.Start();
