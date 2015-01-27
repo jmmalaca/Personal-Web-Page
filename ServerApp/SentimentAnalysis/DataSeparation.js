5abf0b68cce0db00e5ce18d811c9355c27149185
tOOnPT <jose.miguel.malaca@me.com>
2015-01-27 19:26:07 +0000

    Classes and words probabilities calculated. Just in case it needs to be checked.

---
+++ ServerApp/SentimentAnalysis/DataSeparation.js
@@ -0,0 +1,73 @@
+﻿//[Data Separation System: Training Data and Validation(Test) Data]
+
+(function () {
+    
+    "use strict";
+    //[Define your library strictly]
+    
+    function DataSeparation() {
+        
+        //[Private data]
+        var POSITIVE = 'positive';
+        var NEUTRAL = 'neutral';
+        var NEGATIVE = 'negative';
+        
+        var trainingDataPercentage = 70;
+        
+        //select data from the [beginning], from the [middle] or from the [end] of the array
+        var selectDataFrom = 'beginning';
+        
+        var trainData = {};
+        trainData[POSITIVE] = [];
+        trainData[NEUTRAL] = [];
+        trainData[NEGATIVE] = [];
+        
+        var testData = {};
+        testData[POSITIVE] = [];
+        testData[NEUTRAL] = [];
+        testData[NEGATIVE] = [];
+        
+        //[Private Methods]
+        function takeElements(data, classe) {
+            //Math.ceil() = round up result (i.e. 0.7 = 1)
+            var countTraining = Math.ceil((trainingDataPercentage / 100) * data[classe].length);
+            var countValidation = data[classe].length - countTraining;
+            if (selectDataFrom === 'beginning') {
+                trainData[classe] = data[classe].slice(0, countTraining);
+                testData[classe] = data[classe].slice(countTraining, data[classe].length);
+            } else if (selectDataFrom === 'middle') {
+                var divisionInTwo = Math.ceil(countValidation / 2);
+                trainData[classe] = data[classe].slice(divisionInTwo, countTraining);
+                testData[classe].push(data[classe].slice(0, divisionInTwo));
+                testData[classe].push(data[classe].slice((divisionInTwo + countTraining), data[classe].length));
+            } else if (selectDataFrom === 'end') {
+                trainData[classe] = data[classe].slice(0, countValidation);
+                testData[classe] = data[classe].slice(countValidation, data[classe].length);
+            }
+            console.log("  -For training " + classe + "[" + data[classe].length + "] are: " + countTraining + ", and for Validation = " + countValidation);
+        }
+        
+        function separateTrainingAndValidationData(data) {
+            console.log("\n -Percentage data for Training = " + trainingDataPercentage);
+            console.log("  -Slice data from the " + selectDataFrom);
+            takeElements(data, POSITIVE);
+            takeElements(data, NEUTRAL);
+            takeElements(data, NEGATIVE);
+        }
+        
+        //[Public Methods]
+        this.Start = function (data) {
+            
+            separateTrainingAndValidationData(data);
+
+            var separatedData = {};
+            separatedData["train"] = trainData;
+            separatedData["test"] = testData;
+
+            return separatedData;
+        }
+    }
+    
+    //[Export the DataSeparation System Object]
+    module.exports = DataSeparation;
+}());
--- ServerApp/SentimentAnalysis/NaiveBayesClassifier.js
+++ ServerApp/SentimentAnalysis/NaiveBayesClassifier.js
@@ -13,20 +13,89 @@ var fs = require('fs');
         var trainData = {};
         var testData = {};
 
-        var dataClasses = [];
-        var classesProbabilities = {};
-
         //[Private Methods]
-        function trainSystem() {
+        function calcClassesPriorProbabilities() {
             //Classes Prior Probabilities
             //Count total of texts in train data...
+            var totalTexts = trainData['positive'].length + trainData['neutral'].length + trainData['negative'].length;
             //Count total of texts for each class in train data...
+            var totalPositiveTexts = trainData['positive'].length;
+            var totalNeutralTexts = trainData['neutral'].length;
+            var totalNegativeTexts = trainData['negative'].length;
             //Calc probabilities for each class in train data...
-            
+            var classesProbabilities = {};
+            classesProbabilities['positive'] = totalPositiveTexts / totalTexts;
+            classesProbabilities['neutral'] = totalNeutralTexts / totalTexts;
+            classesProbabilities['negative'] = totalNegativeTexts / totalTexts;
+            console.log("  -Prior probability[positive] = " + classesProbabilities['positive'] +
+            "  -Prior probability[neutral] = " + classesProbabilities['neutral'] +
+            "  -Prior probability[negative] = " + classesProbabilities['negative']
+            );
+            return classesProbabilities;
+        }
+        
+        function searchAndCountWordsAvailable() {
+            var wordsList = {};
+            Object.keys(trainData).forEach(function (key) {
+                var texts = trainData[key];
+                texts.forEach(function (text) {
+                    var words = text.split(' ');
+                    words.forEach(function (word) {
+                        if (wordsList.hasOwnProperty(word)) {
+                            wordsList[word] = wordsList[word] + 1;
+                        } else {
+                            wordsList[word] = 1;
+                        }
+                    });
+                });
+            });
+            return wordsList;
+        }
+
+        function searchAndCountWordsClassesAvailable() {
+            var wordsClassesList = {};
+            Object.keys(trainData).forEach(function (key) {
+                var texts = trainData[key];
+                var wordsList = {};
+                texts.forEach(function(text) {
+                    var words = text.split(' ');
+                    words.forEach(function(word) {
+                        if (wordsList.hasOwnProperty(word)) {
+                            wordsList[word] = wordsList[word] + 1;
+                        } else {
+                            wordsList[word] = 1;
+                        }
+                    });
+                });
+                wordsClassesList[key] = wordsList;
+            });
+            return wordsClassesList;
+        }
+
+        function calcWordsPriorProbabilities() {
             //Features Prior Probabilities
             //Count total of features(words) in train data...
+            var wordsTotalData = searchAndCountWordsAvailable();
             //Count total of features(words) in each class of train data...
-            //Calc probabilities for each feature(word) in train data...
+            var wordsData = searchAndCountWordsClassesAvailable();
+
+            var wordsClassePriorProbabilities = {};
+            Object.keys(wordsData).forEach(function(key) {
+                var classeWordsData = wordsData[key];
+                var classewordsProbabilites = {};
+                Object.keys(classeWordsData).forEach(function (word) {
+                    classewordsProbabilites[word] = classeWordsData[word] / wordsTotalData[word];
+                });
+                wordsClassePriorProbabilities[key] = classewordsProbabilites;
+            });
+            return wordsClassePriorProbabilities;
+        }
+
+        function trainSystem() {
+            var classesProbabilities = calcClassesPriorProbabilities();
+            var wordsClassePriorProbabilities = calcWordsPriorProbabilities();
+
+            console.log();
         }
         
         //[Public Methods]
